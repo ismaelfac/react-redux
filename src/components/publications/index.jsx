47edc948675b8e7e5127ec8d69b3870df9252e46
Ismael E. Lastre Alvarez <ismaelfac@gmail.com>
2019-10-23 20:10:53 -0500

    react-redux-actions-json-server

--- src/components/publications/index.jsx
+++ src/components/publications/index.jsx
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
-class Results extends Component {
+class Publications extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            
         };
     }
     render() {
@@ -15,16 +16,15 @@ class Results extends Component {
         } = this.props;
         return (
             <div>
-               <h1>Publications</h1>
+                <h1>Publications</h1>
             </div>                        
         );
     }
 }
 
 const mapStateToProps = state => ({
-    results: state.results
 });
 
 export default withRouter(
-    connect(mapStateToProps)(Results)
+    connect(mapStateToProps)(Publications)
 );
--- src/data/items.js
+++ src/data/items.js
@@ -114,5 +114,5 @@ export default [
         title: `Angular JS`,
         image: `https://s3.amazonaws.com/coursetro/posts/171-full.png`,
         content: `AngularJS is a JavaScript-based open-source front-end web application framework mainly maintained by Google and by a community of individuals and corporations to address many of the challenges encountered in developing single-page applications. The JavaScript components complement Apache Cordova, a framework used for developing cross-platform mobile apps. It aims to simplify both the development and the testing of such applications by providing a framework for client-side model–view–controller (MVC) and model–view–viewmodel (MVVM) architectures, along with components commonly used in rich Internet applications. (This flexibility has led to the acronym MVW, which stands for "model-view-whatever" and may also encompass model–view–presenter and model–view–adapter.) In 2014, the original AngularJS team began working on the Angular application platform.`,
-    },
+    }
 ];
